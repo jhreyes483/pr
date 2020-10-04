359cd7dc93a7e1e1ded32a047e5d58b292bfb814
Javier Reyes <jhreyes483@misena.edu.co>
2020-10-04 09:03:02 -0500

    Shore: integracion sicloud

---
+++ controlador/controladorrutas.php
@@ -0,0 +1,81 @@
+<?php
+
+
+function rutApi(){
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/controlador/controlador.php';
+}
+function rutConIni(){
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.documento.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.usuario.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.rol.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.categoria.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.medida.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.proveedor.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.producto.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.factura.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.modificacion.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.ciudad.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.empresa.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.error.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.telefono.php';
+}
+
+function rutIndex(){
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/plantilla.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/inihtmlN0.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/nav/navgeneralvideo.php';
+}
+
+
+function rutModConexion(){
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/modelo/class.conexion.php';
+   include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/controlador/controladorsession.php';
+}
+
+
+if (  !isset($cons)  ){
+   $cons = true;
+   define('BACKURL','http://localhost/sicloud/back');
+   define('FROMURL','http://localhost/sicloud/from');
+   }
+   
+   function rutIniFromIndex(){
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/plantilla.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/inihtmlN1.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/nav/navgeneralvideo.php';
+   }
+
+
+   function rutIniFromIndexRaiz(){
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/plantilla.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/inihtmlN0.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/nav/navgeneralvideo.php';
+   }
+
+   function rutIniInactiva(){
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/plantilla.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/inihtmlN1.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/nav/nav/navgeneral.php';
+      session_start();
+      session_destroy();
+   }
+
+   function rutFromIni(){
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/plantilla.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/inihtmlN1.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/nav/navN1.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/controlador/controladorsession.php';
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/controlador/controlador.php'; 
+   }
+   
+   function rutFromFin(){ 
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/finhtml.php';
+   }
+   
+   function rutFinFooterFrom(){ 
+      include_once $_SERVER['DOCUMENT_ROOT'].'/sicloud/vista/plantillas/cuerpo/footerN1.php';
+   }
+
+
+
+?>
